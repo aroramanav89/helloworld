02513c8709e6d51b66c1b099076c6c9bac3dff76
Manav <aroramanav89@gmail.com>
2022-09-17 21:17:00 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: __Manav__ Student ID: _____153341219_________ Date: ____17/09/2022____________
+*
+*  Online (Cyclic) URL: __https://drab-erin-nematode-wig.cyclic.app/__
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
